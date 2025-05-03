b3ae031aa18e492e9b78929674b43f231f29b5fd
tomy1375 <tomy_ramos1991@yahoo.com.ar>
2025-05-03 18:12:46 -0300

    FuncionalCorErrores

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    devToolbar: {
+        enabled: false, // Desactiva la toolbar correctamente
+      },
+  integrations: [
+    tailwind(),
+    icon({
+      iconify: {
+        // Solo usamos Lucide
+        collections: {
+          'lucide': () => import('@iconify-json/lucide/icons.json'),
+        }
+      }
+    })
+  ]
+});
